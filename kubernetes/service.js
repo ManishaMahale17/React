82851993fdce6fe1573caf5d3d4a1bcfd5ac4b18
Manisha Mahale <100189110+ManishaMahale17@users.noreply.github.com>
2022-07-05 12:12:02 +0530

    Add files via upload

---
+++ kubernetes/service.js
@@ -0,0 +1,125 @@
+import express from "express";
+
+
+
+const instance = express();
+
+ 
+
+instance.use(express.json());
+
+instance.use(express.urlencoded({extended:false}));
+
+
+
+const port = process.env.PORT || 7022;
+
+
+
+let persons = [
+
+  { PersonId: 101, PersonName: "Manisha", Occupation: "IT", Income: 11000 },
+
+  { PersonId: 102, PersonName: "Vaibhav", Occupation: "HR", Income: 12000 },
+
+  { PersonId: 103, PersonName: "Sangam", Occupation: "IT", Income: 13000 },
+
+  { PersonId: 104, PersonName: "Sweety", Occupation: "HR", Income: 14000 },
+
+  { PersonId: 105, PersonName: "Subhash", Occupation: "IT", Income: 15000 }
+
+];
+
+
+
+// Lets add REST API Methods using HTTP methods of 'instance'
+
+
+
+instance.get("/api/persons", (req, resp) => {
+
+  resp.status(200).send({
+
+    message: "Data reading is successful",
+
+    records: JSON.stringify(persons),
+
+  });
+
+});
+
+instance.get("/api/persons/:id", (req, resp) => {
+
+  // read the URL parameter
+
+  let id = parseInt(req.params.id);
+
+  if (id === 0) {
+
+    resp
+
+      .status(500)
+
+      .send({
+
+        message:
+
+          "Based on Parameter we cannot Process your request, please check data",
+
+      });
+
+  } else {
+
+    let per = persons.find((e, i) => {
+
+      return e.PersonId === id;
+
+    });
+
+    resp
+
+      .status(200)
+
+      .send({
+
+        message: "Data reading is successful",
+
+        record: JSON.stringify(per),
+
+      });
+
+  }
+
+});
+
+instance.post("/api/persons", (req, resp) => {
+
+  // read data from body
+
+  let per = req.body;
+
+  console.log(`Received Data for post is = ${JSON.stringify(per)}`);
+
+  persons.push(per);
+
+  resp
+
+    .status(200)
+
+    .send({
+
+      message: "Data reading is successful",
+
+      record: persons,
+
+    });
+
+});
+
+ 
+
+instance.listen(port, () => {
+
+  console.log(`Server Started on Port ${port}`);
+
+});
