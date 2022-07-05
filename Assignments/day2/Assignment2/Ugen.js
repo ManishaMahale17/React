35a3846eb7a17fbafd6e5639f8507c8a74e7d3b9
Manisha Mahale <100189110+ManishaMahale17@users.noreply.github.com>
2022-07-05 11:45:38 +0530

    Add files via upload

---
+++ Assignments/day2/Assignment2/Ugen.js
@@ -0,0 +1,12 @@
+function fn1()
+{
+   
+    var rd1=document.getElementById("rd1");
+    
+    var rd2=document.getElementById("rd2");
+    var rd2=document.getElementById("rd3");
+    var rd2=document.getElementById("rd4");
+    var rd2=document.getElementById("rd5");
+
+    
+}
